a066ae495406d77f540248bbdecf5df40f74043f
Vasil Vasilev <vvasilev@soft2run.com>
2022-08-31 20:17:25 +0300

    mockawesome added

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,29 @@
 const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'custom-title',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+    },
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+      });
+
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
       return require('./cypress/plugins/index.js')(on, config)
     },
   },
