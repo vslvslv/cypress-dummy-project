a42ccaea96851e6efac6aedcd756b68a117d4120
Vasil Vasilev <vvasilev@soft2run.com>
2022-08-31 22:34:25 +0300

    tests

--- cypress/e2e/google.com/open.cy.js
+++ cypress/e2e/google.com/open.cy.js
@@ -1,11 +1,19 @@
 describe('google.com', () => {
 
     context('tests', () => {
-        it('open', () => {
+        before(() => {
             cy.visit('/')
             cy.get('#search-input > #search').type('lubo zhechev')
             cy.get('#search-icon-legacy').click()
+        })
+
+        it('valid', () => {
             cy.get('#content-section').should('exist')
+        })
+
+        it('fail', () => {
+            
+            cy.get('#content-section').should('not.exist')
         })
     })
     
