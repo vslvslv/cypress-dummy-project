431da543a66b092b0b8a3ad15f4facc2346eafd4
Vasil Vasilev <vvasilev@soft2run.com>
2022-08-31 21:42:55 +0300

    test2

--- cypress/e2e/google.com/open.cy.js
+++ cypress/e2e/google.com/open.cy.js
@@ -3,7 +3,6 @@ describe('google.com', () => {
     context('tests', () => {
         it('open', () => {
             cy.visit('http://google.bg')
-            cy.get('button:Contains("Отхвърляне на всички")').click()
             cy.get('button').should('have.length', 6)
         })
     })
