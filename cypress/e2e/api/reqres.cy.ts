4ea4a490101592231fa40a2ce1df6654f1cba417
Vasil Vasilev <vslvslv89@gmail.com>
2022-12-12 22:28:26 +0200

    migrate to typescript

--- cypress/e2e/api/reqres.cy.ts
+++ cypress/e2e/api/reqres.cy.ts
@@ -1,17 +1,19 @@
+/// <reference types="cypress" />
+
+
 describe("api tests", () => {
-  let baseUrl = "https://reqres.in";
+  let baseUrl: String = "https://reqres.in";
+  let page: Number = 2;
 
   it("get - success", () => {
-    let page = 2;
     cy.api(`${baseUrl}/api/users?page=${page}`).then((response) => {
       expect(response.body.page).to.eq(page);
     });
   });
 
   it("get - failure", () => {
-    let page = 2;
     cy.api(`${baseUrl}/api/users?page=${page}`).then((response) => {
-      expect(response.body.page).to.eq(page + 1);
+      expect(response.body.page).to.eq(3);
     });
   });
   
